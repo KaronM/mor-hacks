c11e3f010671ca4ef6b80f284909393a483517ca
nyajahshirley <166248155+nyajahshirley@users.noreply.github.com>
2024-04-07 11:50:05 -0400

    complete resources

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -3,20 +3,24 @@ import React, { useState } from 'react';
 const Resources = () => {
     
     return (
-        <div>
+        <div style={{ backgroundColor: '#FFC0CB', padding: '20px' }}>
             <h1>In Resources!</h1>
             <br></br>
-            <h2>Better Understand the Different Types of Sickle Cell Disease variations </h2>
+            <h2 style={{ color: 'dark red' }}>Better Understand the Different Types of Sickle Cell Disease variations </h2>
             <p> Sickle cell anemia arises when an individual inherits two copies of the HbS gene, one from each parent. This particular manifestation of sickle cell disease tends to be more severe than other forms, necessitating a more rigorous approach to medical management due to its heightened propensity for complications and adverse health outcomes... <a href="https://www.mayoclinic.org/diseases-conditions/sickle-cell-anemia/symptoms-causes/syc-20355876">Learn more</a> </p>
             <br></br>
             <p>Sickle cell hemoglobin arises from the inheritance of one HbS gene from one parent and one HbC gene from the other. This unique genetic combination often yields less severe symptoms than sickle cell anemia, though complications such as acute chest syndrome and splenic sequestration remain concerns. Additionally, individuals with hemoglobin SC disease may experience higher incidence of vision-related issues attributed to sickle cell retinopathy... <a href="https://www.rarediseaseadvisor.com/hcp-resource/sickle-cell-disease-genetics/">Learn more</a> </p>
             <br></br>
             <p>Sickle Bata Thalassemia occur when a person inhertits a combination of one gene for sickle hemoglobin and one gene for thalassemia. This genetic amalgamation gives rise to a nuanced clinical profile, marked by a wide array of symptoms that can vary in intensity. Individuals with sickle beta thalassemia may encounter a broad spectrum of health challenges, ranging from milder manifestations to more severe presentations. These symptoms may include complications such as hand and foot swelling, aplastic crisis, etc... <a href="https://dph.illinois.gov/content/dam/soi/en/web/idph/files/publications/factsheetpscbetathal.pdf">Learn more</a> </p> 
             <br></br>
-            <h2>Sickle cell Triggers</h2>
+            <h2 style={{ color: 'dark red' }}>Sickle cell Triggers</h2>
             <br></br>
             <p>Exposure to extreme temperatures can be troubling for those with sickle cell disease. When it's too cold, blood flow slows down, leading to painful crisis. On the flip side, temperatures too hot without enough water thickens the blood, promoting the sickling of red blood cells and more pain. </p>
             <p>But it's not just the weather. Both physical and emotional stress can also trigger pain crisis. Too much physical activity tires out muscles and makes the body demand more oxygen, while emotional stress releases hormones that can worsen sickle cell symptoms... <a href="https://www.nhlbi.nih.gov/health/sickle-cell-disease/health-effects" >Learn more</a> </p>
+            <br></br>
+            <h2 style={{ color: 'dark red' }}>How to prevent/manage sickle cell crisis?</h2>
+            <br></br>
+            <p>Although it's challenging to entirely prevent a sickle cell crisis, there are strategies to alleviate the risk. Firstly, maintaining hydration and avoiding strenuous exercise can help. Dressing appropriately for the weather is crucial to regulate body temperature. Managing stress effectively is also important, as stress can exacerbate symptoms. Additionally, taking over-the-counter medications as recommended by a healthcare provider can provide relief and manage symptoms during a crisis...<a href="https://www.healthline.com/health/sickle-cell-crisis-triggers#prevention"> Learn more</a> </p>
              
         </div>
     );
