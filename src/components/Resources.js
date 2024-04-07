a39a346543197f10381609e2a447f85398e98d79
nyajahshirley <166248155+nyajahshirley@users.noreply.github.com>
2024-04-07 09:11:59 -0400

    completed resources

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -4,7 +4,20 @@ const Resources = () => {
     
     return (
         <div>
-            <p>In Resources!</p>
+            <h1>In Resources!</h1>
+            <br></br>
+            <h2>Better Understand the Different Types of Sickle Cell Disease variations </h2>
+            <p> Sickle cell anemia arises when an individual inherits two copies of the HbS gene, one from each parent. This particular manifestation of sickle cell disease tends to be more severe than other forms, necessitating a more rigorous approach to medical management due to its heightened propensity for complications and adverse health outcomes... <a href="https://www.mayoclinic.org/diseases-conditions/sickle-cell-anemia/symptoms-causes/syc-20355876">Learn more</a> </p>
+            <br></br>
+            <p>Sickle cell hemoglobin arises from the inheritance of one HbS gene from one parent and one HbC gene from the other. This unique genetic combination often yields less severe symptoms than sickle cell anemia, though complications such as acute chest syndrome and splenic sequestration remain concerns. Additionally, individuals with hemoglobin SC disease may experience higher incidence of vision-related issues attributed to sickle cell retinopathy... <a href="https://www.rarediseaseadvisor.com/hcp-resource/sickle-cell-disease-genetics/">Learn more</a> </p>
+            <br></br>
+            <p>Sickle Bata Thalassemia occur when a person inhertits a combination of one gene for sickle hemoglobin and one gene for thalassemia. This genetic amalgamation gives rise to a nuanced clinical profile, marked by a wide array of symptoms that can vary in intensity. Individuals with sickle beta thalassemia may encounter a broad spectrum of health challenges, ranging from milder manifestations to more severe presentations. These symptoms may include complications such as hand and foot swelling, aplastic crisis, etc... <a href="https://dph.illinois.gov/content/dam/soi/en/web/idph/files/publications/factsheetpscbetathal.pdf">Learn more</a> </p> 
+            <br></br>
+            <h2>Sickle cell Triggers</h2>
+            <br></br>
+            <p>Exposures to  Extreme cold temperatures can reduce blood flow causing pain crises. Exposure to extreme heat along with dehydration can increase the vicosity of blood and promte sickling of red blood cells; which also leads to pain crises. </p>
+            <p>Emotinal or physical stress such as intense physical activity are also triggers. An extensive amout of physical activity can lead to muscle fatigueand increase the demand for oxygen; emotinal stress can lead to te production of stress harmones, all of this can promote sickleing of red blood cells and pian crises.  </p>
+             
         </div>
     );
 };
